9b62a4b253edb7c8f6280b79036ab48d2a39e5f3
Akmal1796 <mhdakmal1796@gmail.com>
2024-07-15 18:50:01 +0530

    Import all the components to the index.js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+/* Import all componenets and functions */
+
 import { sidebar } from "./sidebar.js";
+import { api_key, imageBaseURL, fetchDataFromServer } from "./api.js";
 
 sidebar();
