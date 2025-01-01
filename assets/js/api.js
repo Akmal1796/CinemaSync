2917d6d1451595dab0068ed1496a3749e999ff97
Akmal1796 <mhdakmal1796@gmail.com>
2025-01-01 09:50:39 +0530

    Updated API key

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const api_key = '0917b6b024e83a3077b01ef3b5f49348';
+const api_key = 'a4d0dea6657fe3e2d6bd991513a34da8';
 const imageBaseURL = 'https://image.tmdb.org/t/p/';
 
 /* Fetch Data from a server using the 'url' and passes the result in JSON to a 'callback' function, along with an optional parameter if has 'optionalParam'. */
 
-const fetchDataFromServer = function(url, callback, optionalParam) {
+const fetchDataFromServer = function (url, callback, optionalParam) {
     fetch(url)
-    .then(response => response.json())
-    .then(data => callback(data, optionalParam));
+        .then(response => response.json())
+        .then(data => callback(data, optionalParam));
 }
 
 export { imageBaseURL, api_key, fetchDataFromServer };
